913659849710585f50d1093a9a77ddb511e60564
RockBobster <github@careyclan.co.uk>
2018-03-03 16:34:34 +0000

    laptop migration

--- download.py
+++ download.py
@@ -1,22 +1,23 @@
 import requests
 import re
+import mysql.connector
 
 #Rightmove Regions
-#Somerset,Dorset,Wiltshire,Devon
-#rmRegion=["REGION%5E61322","REGION%5E61298","REGION%5E61328","REGION%5E61297"]
-
-rmRegion=["REGION%5E61297"]
+rmRegion=([["REGION%5E61297","Devon"],
+           ["REGION%5E61322","Somerset"],
+           ["REGION%5E61298","Dorset"],
+           ["REGION%5E61328","Wiltshire"]])
 
 
 baseurl='http://www.rightmove.co.uk/property-for-sale/find.html?'
-baseurl += '&maxPrice=250000'
+baseurl += '&maxPrice=150000'
 baseurl += '&propertyTypes=detached'
 baseurl += '&maxDaysSinceAdded=14'
 
 session = requests.Session()
 session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36'})
 
-for region in rmRegion:
+for region,county in rmRegion:
     #append specific region and page number for page 1 of region
     url = baseurl + '&locationIdentifier=' + region + '&index=0'
     response = session.get(url)
@@ -26,7 +27,6 @@ for region in rmRegion:
     m = p.search(str(response.content))
     recCount = int(m.group(1))
     propIDList=[]
-
 
     for page in range(0,recCount,24):
         url = baseurl + '&locationIdentifier=' + region + '&index=' + str(page)
@@ -39,19 +39,60 @@ for region in rmRegion:
         for result in resultSet:
             propIDList.append(result.group(1))
 
+    #Create mySQL connector and base insert string
+    cnx = mysql.connector.connect(user='scrapper', password='!Nsert23', host='127.0.0.1', database='property')
+    cursor = cnx.cursor()
+    addProp = ("INSERT INTO property.houses "
+               "(reseller,EA,url,county,postcode,latitude,longitude,propertyID,propertyType,propertySubType,price,beds,dateAdded) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+
+    # Loop over each property to download full details
     for prop in propIDList:
-        print(prop)
-        print(propIDList[int(prop)])
+
+        # Download the property details page for prop
+        url='http://www.rightmove.co.uk/property-for-sale/property-' + prop + '.html'
+        response = str(session.get(url).content)
+
+        # Strip out the short and long decscription
+        shortDesc = re.search("\<title\>([\w \,]+)\<\/title\>",response).group(1)
+        fullDesc = re.search("\<p itemprop=\"description\"\>(.+?)\<\/p\>",response).group(1)
+        EA = re.search("companyName\":\"(.+?)\"",response).group(1)
+
+        # Grab substring containing key data
+        propdetails = re.search("{\"location\":{(.+)selectedCurrency",response).group(1)
+
+        # Now examine the substring and get that data
+        postcode = re.search("postcode\":\"(\w+ \w+)\"",propdetails).group(1)
+        latitude = re.search("latitude\":([-\d\.]+)",propdetails).group(1)
+        longitude = re.search("longitude\":([-\d\.]+)", propdetails).group(1)
+        propertyID = re.search("propertyId\":([\d]+)", propdetails).group(1)
+        propertyType = re.search("propertyType\":\"([\w]+)", propdetails).group(1)
+        propertySubType = re.search("propertySubType\":\"([\w]+)", propdetails).group(1)
+        price = re.search("price\":([\d\.]+)", propdetails).group(1)
+        beds = re.search("beds\":([\d]+)", propdetails).group(1)
+        dateAdded = re.search("added\":\"([\d]+)", propdetails).group(1)
+
+        cursor.execute(addProp,('Rightmove',EA,url,county,postcode,latitude,longitude,propertyID,propertyType,propertySubType,price,beds,dateAdded))
+        cnx.commit()
 
 
+    cursor.close()
+    cnx.close()
 
-#http://www.rightmove.co.uk/property-for-sale/property-51283944.html
+
 
 #print(session.headers)
 #print(response.headers)
 #print(response.status_code)
 #print(pages)
 
-        #f = open('workfile.html', 'w')
-#f.write(str(response.content))
+#{"location":
+#{"postcode":"EX20 2DE","country":"GB","latitude":50.80104270685834,"longitude":-3.89710189589049}
+        # ,"propertyId":49215366,"viewType":"Current","imageCount":9,"floorplanCount":1,"videoProvider":"Embedded","propertyInfo":
+        # {"propertyType":"Houses","propertySubType":"Cottage","price":189995.0,"beds":3,"added":"20170706","soldSTC":false,"retirement":null,"preOwned":"Resale","ownership":"Non-shared ownership","auctionOnly":false,"letAgreed":false,"lettingType":null,"furnishedType":null,"minSizeFt":null,"maxSizeFt":null,"minSizeAc":null,"maxSizeAc":null,"businessForSale":false,"priceQualifier":"Offers in Region of","currency":"GBP","selectedPrice":null,"selectedCurrency":null
+        # }
+        # }
+
+#f = open(prop + '.html', 'w')
+#f.write(response)
 #f.close()
